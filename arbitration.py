7fc9dcbd7a91604179ea0d818f54dbf437312afe
iDandelions <neil@iDandelions.com>
2013-03-07 16:28:40 +0000

    Expanded the README and added some comments to arbitration class.

--- arbitration.py
+++ arbitration.py
@@ -1,3 +1,4 @@
+# A single bet object
 class Bet(object):
     
     def __init__(self,name):
@@ -9,7 +10,7 @@ class Bet(object):
         id += ' at ' + '%.2f' % self.odds
         return id
         
-        
+# Arbitration bet, contains two bets, a lay and a place, which form both sides of the arbitration
 class ArbitrationBet(object):
     
     def __init__(self,name):
@@ -26,10 +27,14 @@ class ArbitrationBet(object):
         id += '%.2f%%' % ((self.layBet.stake - self.placeBet.stake) / self.placeBet.stake * 100)
         return id    
         
+    # Calculate the lay bet stake needed to equalize the profit on both a win and a lose
+    # The place bet and the lay bet odds need to be initialise before calling
     def calculateLayBetStake(self,odds):
         self.layBet.odds  = odds
         self.layBet.stake = round(((self.placeBet.stake*self.placeBet.odds)+self.placeBet.stake)/(1+odds),2)
         
+    # Calculate the place bet stake needed to equalize the profit on both a win and a lose
+    # The lay bet and the place bet odds need to be initialise before calling
     def calculatePlaceBetStake(self,odds):
         self.placeBet.odds  = odds
         self.placeBet.stake = round(((self.LayBet.stake*self.LayBet.odds)+self.LayBet.stake)/(1+odds),2)
